21418399df349358165b2efd4af802945d8e835a
eydissylvia <ese20@hi.is>
2019-11-14 21:23:50 +0000

    bæti við hjálparföllum úr fyrirlestrum

--- scripts.js
+++ scripts.js
@@ -4,9 +4,45 @@ const API_URL = 'https://apis.is/company?name=';
  * Leit að fyrirtækjum á Íslandi gegnum apis.is
  */
 const program = (() => {
+  let companiesSection;
+
   function init(companies) {
+    companiesSection = companies;
 
   }
+
+
+  function fetchData() {
+
+  }
+
+  function show() {
+    
+  }
+
+
+
+  // fengið úr fyrirlestri 11
+  function empty(el) {
+    while (el.firstChild) {
+      el.removeChild(el.firstChild);
+    }
+  }
+
+
+  // fengið úr fyrirlestri 10
+  function el(name, ...children) {
+    const element = document.createElement(name);
+    for (const child of children) {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else {
+        element.appendChild(child);
+      }
+    }
+    return element;
+  }
+
 
   return {
     init,
@@ -14,5 +50,6 @@ const program = (() => {
 })();
 
 document.addEventListener('DOMContentLoaded', () => {
+  const companies = document.querySelector('.companies');
   program.init(companies);
 });
